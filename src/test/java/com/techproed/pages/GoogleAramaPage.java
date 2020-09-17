af2a68eb65a721f5cd595c21f98b2da9da21013f
Alper <alpereneskar@gmail.com>
2020-09-17 22:17:57 +0200

    day24 sonu

---
+++ src/test/java/com/techproed/pages/GoogleAramaPage.java
@@ -0,0 +1,19 @@
+package com.techproed.pages;
+
+import com.techproed.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class GoogleAramaPage {
+    public GoogleAramaPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+    @FindBy (name = "q")
+    public WebElement aramaKutusu;
+
+    @FindBy (tagName = "a") // sayfadaki butun linkleri alabiliyoruz
+    public List<WebElement> tumLinkler;
+}
